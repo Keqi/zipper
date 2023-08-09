120b7014db8ec6b0a67aa3db9234c810e7f8c93b
Maciej Nowak <maciej.nowak90@gmail.com>
2023-08-09 11:05:01 +0200

    Chore: Final clean-up

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :user do
-    username { 'maciej.nowak' }
-    password { 'Abcde12345!!' }
+    username { Faker::Internet.username }
+    password { Faker::Internet.password(min_length: 12, special_characters: true) }
   end
 end
